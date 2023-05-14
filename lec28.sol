0e0198da0abd1bfde2d4db5cdaa371f7a18b1127
Jaesung Ryu <cwryu6252@naver.com>
2023-05-14 19:32:54 +0900

    lec28 - try/catch 2

--- lec28.sol
+++ lec28.sol
@@ -28,3 +28,21 @@ contract runner {
         }
     }
 }
+
+// 스마트 컨트랙 내에서 함수를 부를 때
+contract runner2 {
+    event catchOnly(string _name, string _err);
+
+    function simple() public returns (uint) {
+        return 4;
+    }
+
+    function playTryCatch() public returns (uint, bool) {
+        try this.simple() returns (uint _value) {
+            return (_value, true);
+        } catch {
+            emit catchOnly("catch", "Error");
+            return (0, false);
+        }
+    }
+}
